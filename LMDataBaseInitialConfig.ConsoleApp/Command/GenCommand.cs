b783a91577fa0e7a7e21aaab8ea49d104a25c169
Everson Rafael Moura <eversonrafael.moura@gmail.com>
2017-10-04 10:13:04 -0300

    Create config interfaces

---
+++ LMDataBaseInitialConfig.ConsoleApp/Command/GenCommand.cs
@@ -0,0 +1,34 @@
+﻿using LMDataBaseInitialConfig.ConsoleApp.ConfigHelper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMDataBaseInitialConfig.ConsoleApp
+{
+    public class GenCommand : ICommand
+    {
+        private IConfigHelpter _ConfigHelpter;
+
+        public GenCommand(IConfigHelpter configHelpter)
+        {
+
+            this._ConfigHelpter = configHelpter;
+
+        }
+
+        public bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ LMDataBaseInitialConfig.ConsoleApp/ConfigHelper/IConfigHelpter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMDataBaseInitialConfig.ConsoleApp.ConfigHelper
+{
+
+    public interface IConfigHelpter
+    {
+
+        string GetConn(string key);
+        string SetConn(string key);
+        void RemoveConn(string key);
+        List<string> GetConnKeys();
+
+        string GetTable(string key);
+        string SetTable(string key);
+        void RemoveTable(string key);
+        List<string> GetTableKeys();
+
+
+    }
+
+}
